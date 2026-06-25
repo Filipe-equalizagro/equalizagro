ece6859dc9b8217d867c8e6d8a24a1dc9142eb64
Hevellyntecn <hevellyntecn@gmail.com>
2026-06-25 11:19:16 -0300

    Atualização 25/06 1

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  async redirects() {
+    return [
+      {
+        source: '/ferramentas/go2apply',
+        destination: '/pulverizacao/go2apply',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
